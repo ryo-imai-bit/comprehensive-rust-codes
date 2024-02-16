f13d1b0d6da4beee3bd543ad087d558e12b36351
ryo-imai-bit <mayoimaimai9@gmail.com>
2024-02-17 03:55:33 +0900

    day3 morning

--- src/day3/main.rs
+++ src/day3/main.rs
@@ -76,6 +76,135 @@ impl PackageBuilder {
     }
 }
 
+/// A node in the binary tree.
+#[derive(Debug)]
+struct Node<T: Ord> {
+    value: T,
+    left: Subtree<T>,
+    right: Subtree<T>,
+}
+
+/// A possibly-empty subtree.
+#[derive(Debug)]
+struct Subtree<T: Ord>(Option<Box<Node<T>>>);
+
+/// A container storing a set of values, using a binary tree.
+///
+/// If the same value is added multiple times, it is only stored once.
+#[derive(Debug)]
+pub struct BinaryTree<T: Ord> {
+    root: Subtree<T>,
+}
+
+// Implement `new`, `insert`, `len`, and `has`.
+impl<T: Ord> BinaryTree<T> {
+    pub fn new() -> Self {
+        Self { root: Subtree(None) }
+    }
+
+    pub fn insert(&mut self, value: T) {
+        self.root.insert(value);
+    }
+
+    pub fn len(&self) -> usize {
+        self.root.len()
+    }
+
+    pub fn has(&self, value: &T) -> bool {
+        self.root.has(value)
+    }
+}
+
+impl<T: Ord> Subtree<T> {
+    fn insert(&mut self, value: T) {
+        match &mut self.0 {
+            Some(node) => {
+                if value < node.value {
+                    node.left.insert(value);
+                } else if value > node.value {
+                    node.right.insert(value);
+                }
+            },
+            None => {
+                self.0 = Some(Box::new(Node {
+                    value,
+                    left: Subtree(None),
+                    right: Subtree(None),
+                }));
+            }
+        }
+    }
+
+    fn len(&self) -> usize {
+        match &self.0 {
+            Some(node) => 1 + node.left.len() + node.right.len(),
+            None => 0,
+        }
+    }
+
+    fn has(&self, value: &T) -> bool {
+        match &self.0 {
+            Some(node) => {
+                if value < &node.value {
+                    node.left.has(value)
+                } else if value > &node.value {
+                    node.right.has(value)
+                } else {
+                    true
+                }
+            },
+            None => false,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn len() {
+        let mut tree = BinaryTree::new();
+        assert_eq!(tree.len(), 0);
+        tree.insert(2);
+        assert_eq!(tree.len(), 1);
+        tree.insert(1);
+        assert_eq!(tree.len(), 2);
+        tree.insert(2); // not a unique item
+        assert_eq!(tree.len(), 2);
+    }
+
+    #[test]
+    fn has() {
+        let mut tree = BinaryTree::new();
+        fn check_has(tree: &BinaryTree<i32>, exp: &[bool]) {
+            let got: Vec<bool> =
+                (0..exp.len()).map(|i| tree.has(&(i as i32))).collect();
+            assert_eq!(&got, exp);
+        }
+
+        check_has(&tree, &[false, false, false, false, false]);
+        tree.insert(0);
+        check_has(&tree, &[true, false, false, false, false]);
+        tree.insert(4);
+        check_has(&tree, &[true, false, false, false, true]);
+        tree.insert(4);
+        check_has(&tree, &[true, false, false, false, true]);
+        tree.insert(3);
+        check_has(&tree, &[true, false, false, true, true]);
+    }
+
+    #[test]
+    fn unbalanced() {
+        let mut tree = BinaryTree::new();
+        for i in 0..100 {
+            tree.insert(i);
+        }
+        assert_eq!(tree.len(), 100);
+        assert!(tree.has(&50));
+    }
+}
+
 fn main() {
     let base64 = PackageBuilder::new("base64").version("0.13").build();
     println!("base64: {base64:?}");
@@ -89,4 +218,10 @@ fn main() {
         .dependency(log.as_dependency())
         .build();
     println!("serde: {serde:?}");
+
+    let mut tree = BinaryTree::new();
+    tree.insert("foo");
+    assert_eq!(tree.len(), 1);
+    tree.insert("bar");
+    assert!(tree.has(&"foo"));
 }
