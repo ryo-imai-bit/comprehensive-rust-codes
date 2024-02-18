aee1ce36acd55e681d928e21929c32e6b202bc51
ryo-imai-bit <mayoimaimai9@gmail.com>
2024-02-18 20:42:45 +0900

    day4 morning

---
+++ src/day4/main.rs
@@ -0,0 +1,103 @@
+/// Calculate the differences between elements of `values` offset by `offset`,
+/// wrapping around from the end of `values` to the beginning.
+///
+/// Element `n` of the result is `values[(n+offset)%len] - values[n]`.
+fn offset_differences<N>(offset: usize, values: Vec<N>) -> Vec<N>
+where
+    N: Copy + std::ops::Sub<Output = N>,
+{
+    values.iter().enumerate().map(|(i, &v)| values[(i + offset) % values.len()] - v).collect()
+}
+
+#[test]
+fn test_offset_one() {
+    assert_eq!(offset_differences(1, vec![1, 3, 5, 7]), vec![2, 2, 2, -6]);
+    assert_eq!(offset_differences(1, vec![1, 3, 5]), vec![2, 2, -4]);
+    assert_eq!(offset_differences(1, vec![1, 3]), vec![2, -2]);
+}
+
+#[test]
+fn test_larger_offsets() {
+    assert_eq!(offset_differences(2, vec![1, 3, 5, 7]), vec![4, 4, -4, -4]);
+    assert_eq!(offset_differences(3, vec![1, 3, 5, 7]), vec![6, -2, -2, -2]);
+    assert_eq!(offset_differences(4, vec![1, 3, 5, 7]), vec![0, 0, 0, 0]);
+    assert_eq!(offset_differences(5, vec![1, 3, 5, 7]), vec![2, 2, 2, -6]);
+}
+
+#[test]
+fn test_custom_type() {
+    assert_eq!(
+        offset_differences(1, vec![1.0, 11.0, 5.0, 0.0]),
+        vec![10.0, -6.0, -5.0, 1.0]
+    );
+}
+
+#[test]
+fn test_degenerate_cases() {
+    assert_eq!(offset_differences(1, vec![0]), vec![0]);
+    assert_eq!(offset_differences(1, vec![1]), vec![0]);
+    let empty: Vec<i32> = vec![];
+    assert_eq!(offset_differences(1, empty), vec![]);
+}
+
+mod widgets;
+use widgets::{Window, Label, Button, Widget};
+
+
+// luhn algorithm
+pub fn luhn(cc_number: &str) -> bool {
+    let mut sum = 0;
+    let mut double = false;
+    let mut digits = 0;
+
+    for c in cc_number.chars().rev() {
+        if let Some(digit) = c.to_digit(10) {
+            digits += 1;
+            if double {
+                let double_digit = digit * 2;
+                sum +=
+                    if double_digit > 9 { double_digit - 9 } else { double_digit };
+            } else {
+                sum += digit;
+            }
+            double = !double;
+        } else if c.is_whitespace() {
+            continue;
+        } else {
+            return false;
+        }
+    }
+
+    digits >= 2 && sum % 10 == 0
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_valid_cc_number() {
+        assert!(luhn("4263 9826 4026 9299"));
+        assert!(luhn("4539 3195 0343 6467"));
+        assert!(luhn("7992 7398 713"));
+    }
+
+    #[test]
+    fn test_invalid_cc_number() {
+        assert!(!luhn("4223 9826 4026 9299"));
+        assert!(!luhn("4539 3195 0343 6476"));
+        assert!(!luhn("8273 1232 7352 0569"));
+        assert!(!luhn("foo"));
+    }
+}
+
+
+fn main() {
+    println!("{:?}", offset_differences(1, vec![1.0, 11.0, 5.0, 0.0]));
+
+    // modules
+    let mut window = Window::new("Rust GUI Demo 1.23");
+    window.add_widget(Box::new(Label::new("This is a small text GUI demo.")));
+    window.add_widget(Box::new(Button::new("Click me!")));
+    window.draw();
+}
